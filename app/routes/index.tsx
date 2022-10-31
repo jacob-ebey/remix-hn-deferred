a419355b8df7da508246ec070d8c0a193210be85
Jacob Ebey <jacob.ebey@live.com>
2022-10-31 16:28:51 -0700

    chore: make everything deferred

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,9 @@
-import type { LoaderFunction } from "@remix-run/cloudflare";
-import { json } from "@remix-run/cloudflare";
-import { useLoaderData, Link } from "@remix-run/react";
+import { Suspense } from "react";
+import { type LoaderArgs, defer } from "@remix-run/cloudflare";
+import { useLoaderData, Await, Link } from "@remix-run/react";
 
 import { getStories } from "~/api.server";
-import type { Story as StoryType } from "~/api.server";
 import Story from "~/components/story";
-
-interface StoriesData {
-  page: number;
-  type: string;
-  stories: StoryType[];
-}
 
 const mapStories: Record<string, string> = {
   top: "news",
@@ -20,13 +13,17 @@ const mapStories: Record<string, string> = {
   job: "jobs",
 };
 
-export let loader: LoaderFunction = async ({ params, request }) => {
+export async function loader({ params, request }: LoaderArgs) {
   let url = new URL(request.url);
   let page = +(url.searchParams.get("page") || 1);
   const type = params.category || "top";
-  const stories = await getStories(mapStories[type], page);
+  let stories = getStories(mapStories[type], page);
 
-  return json(
+  if (url.searchParams.has("await")) {
+    stories = (await stories) as any;
+  }
+
+  return defer(
     { type, stories, page },
     {
       headers: {
@@ -34,53 +31,74 @@ export let loader: LoaderFunction = async ({ params, request }) => {
       },
     }
   );
-};
+}
 
 export default function Stories() {
-  let { page, type, stories } = useLoaderData<StoriesData>();
+  let { page, type, stories } = useLoaderData<typeof loader>();
 
   return (
     <div className="news-view">
-      <div className="news-list-nav">
-        {page > 1 ? (
-          <Link
-            className="page-link"
-            to={`/${type}?page=${page - 1}`}
-            aria-label="Previous Page"
-            prefetch="intent"
-          >
-            {"<"} prev
-          </Link>
-        ) : (
-          <span className="page-link disabled" aria-disabled="true">
-            {"<"} prev
-          </span>
-        )}
-        <span>page {page}</span>
-        {stories && stories.length >= 29 ? (
-          <Link
-            className="page-link"
-            to={`/${type}?page=${page + 1}`}
-            aria-label="Next Page"
-            prefetch="intent"
-          >
-            more {">"}
-          </Link>
-        ) : (
-          <span className="page-link disabled" aria-disabled="true">
-            more {">"}
-          </span>
-        )}
-      </div>
-      <main className="news-list">
-        {stories && (
-          <ul>
-            {stories.map((story) => (
-              <Story key={story.id} story={story} />
-            ))}
-          </ul>
-        )}
-      </main>
+      <Suspense
+        fallback={
+          <div className="news-list-nav">
+            <span className="page-link disabled" aria-disabled="true">
+              {"<"} prev
+            </span>
+            <span>page {page}</span>
+
+            <span className="page-link disabled" aria-disabled="true">
+              more {">"}
+            </span>
+          </div>
+        }
+      >
+        <Await resolve={stories}>
+          {(stories) => (
+            <>
+              <div className="news-list-nav">
+                {page > 1 ? (
+                  <Link
+                    className="page-link"
+                    to={`/${type}?page=${page - 1}`}
+                    aria-label="Previous Page"
+                    prefetch="intent"
+                  >
+                    {"<"} prev
+                  </Link>
+                ) : (
+                  <span className="page-link disabled" aria-disabled="true">
+                    {"<"} prev
+                  </span>
+                )}
+                <span>page {page}</span>
+                {stories && stories.length >= 29 ? (
+                  <Link
+                    className="page-link"
+                    to={`/${type}?page=${page + 1}`}
+                    aria-label="Next Page"
+                    prefetch="intent"
+                  >
+                    more {">"}
+                  </Link>
+                ) : (
+                  <span className="page-link disabled" aria-disabled="true">
+                    more {">"}
+                  </span>
+                )}
+              </div>
+              <main className="news-list">
+                {stories && (
+                  <ul>
+                    {stories.map((story) => (
+                      <Story key={story.id} story={story} />
+                    ))}
+                  </ul>
+                )}
+              </main>
+            </>
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
 }
